cbafddd40c74606c1377bbc96305a13a81c83e46
Ukkkkkkkk <tea1328@gmail.com>
2024-02-22 16:13:58 +0900

    dev 브렌치 생성 및 코드변경

--- src/main/java/com/example/GitTest/TestController.java
+++ src/main/java/com/example/GitTest/TestController.java
@@ -9,6 +9,7 @@ public class TestController {
     @GetMapping("/main")
     public String main(){
         System.out.println("집에서 수정함");
+        System.out.println("이 코드는 dev branch 에서 추가됨");
         return "main";
     }
 }
